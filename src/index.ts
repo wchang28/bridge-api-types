dcdae807d30381b9a4ec0da6c5ff635a227875a8
wchang28 <wchang28@hotmail.com>
2019-01-04 15:06:48 -0500

    added more comment

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export type ErrorResponse = BundleResponse<Error>;
 
 export type StateCode = ('AK' | 'AL' | 'AR' | 'AZ' | 'CA' | 'CO' | 'CT' | 'DC' | 'DE' | 'FL' | 'GA' | 'GU' | 'HI' | 'IA' | 'ID' | 'IL' | 'IN' | 'KS' | 'KY' | 'LA' | 'MA' | 'MD' | 'ME' | 'MI' | 'MN' | 'MO' | 'MS' | 'MT' | 'NC' | 'ND' | 'NE' | 'NH' | 'NJ' | 'NM' | 'NV' | 'NY' | 'OH' | 'OK' | 'OR' | 'PA' | 'PR' | 'RI' | 'SC' | 'SD' | 'TN' | 'TX' | 'UT' | 'VA' | 'VT' | 'WA' | 'WI' | 'WV' | 'WY');
 
-export type GeoCoordinate = [number, number];
+export type GeoCoordinate = [number, number];   // [longitude, latitude]
 
 export namespace PublicData {
 
